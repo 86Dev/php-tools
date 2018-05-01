9960dde4461416960df05f2eb182cf9d4fa9f18a
jtisseau <jonathan.tisseau@86dev.fr>
2018-05-01 17:38:14 +0200

    bugfix: inverted expected and actual values

--- tests/BoolHelperTest.php
+++ tests/BoolHelperTest.php
@@ -7,111 +7,113 @@ final class BoolHelperTest extends TestCase
 {
 	public function testIsTrueString()
 	{
-		$this->assertEquals(BoolHelper::is_true_string('true'), true);
-		$this->assertEquals(BoolHelper::is_true_string('yes'), true);
-		$this->assertEquals(BoolHelper::is_true_string('vrai'), true);
-		$this->assertEquals(BoolHelper::is_true_string('oui'), true);
+		$this->assertEquals(true, BoolHelper::is_true_string('true'));
+		$this->assertEquals(true, BoolHelper::is_true_string('yes'));
+		$this->assertEquals(true, BoolHelper::is_true_string('vrai'));
+		$this->assertEquals(true, BoolHelper::is_true_string('oui'));
 
-		$this->assertEquals(BoolHelper::is_true_string('True'), true);
-		$this->assertEquals(BoolHelper::is_true_string('Yes'), true);
-		$this->assertEquals(BoolHelper::is_true_string('Vrai'), true);
-		$this->assertEquals(BoolHelper::is_true_string('Oui'), true);
+		$this->assertEquals(true, BoolHelper::is_true_string('True'));
+		$this->assertEquals(true, BoolHelper::is_true_string('Yes'));
+		$this->assertEquals(true, BoolHelper::is_true_string('Vrai'));
+		$this->assertEquals(true, BoolHelper::is_true_string('Oui'));
 
-		$this->assertEquals(BoolHelper::is_true_string('TRUE'), true);
-		$this->assertEquals(BoolHelper::is_true_string('YES'), true);
-		$this->assertEquals(BoolHelper::is_true_string('VRAI'), true);
-		$this->assertEquals(BoolHelper::is_true_string('OUI'), true);
+		$this->assertEquals(true, BoolHelper::is_true_string('TRUE'));
+		$this->assertEquals(true, BoolHelper::is_true_string('YES'));
+		$this->assertEquals(true, BoolHelper::is_true_string('VRAI'));
+		$this->assertEquals(true, BoolHelper::is_true_string('OUI'));
 
-		$this->assertEquals(BoolHelper::is_true_string('1'), true);
-		$this->assertEquals(BoolHelper::is_true_string('0'), false);
+		$this->assertEquals(true, BoolHelper::is_true_string('1'));
+		$this->assertEquals(false, BoolHelper::is_true_string('0'));
 
-		$this->assertEquals(BoolHelper::is_true_string('false'), false);
-		$this->assertEquals(BoolHelper::is_true_string('no'), false);
-		$this->assertEquals(BoolHelper::is_true_string('faux'), false);
-		$this->assertEquals(BoolHelper::is_true_string('non'), false);
+		$this->assertEquals(false, BoolHelper::is_true_string('false'));
+		$this->assertEquals(false, BoolHelper::is_true_string('no'));
+		$this->assertEquals(false, BoolHelper::is_true_string('faux'));
+		$this->assertEquals(false, BoolHelper::is_true_string('non'));
 
-		$this->assertEquals(BoolHelper::is_true_string('False'), false);
-		$this->assertEquals(BoolHelper::is_true_string('No'), false);
-		$this->assertEquals(BoolHelper::is_true_string('Faux'), false);
-		$this->assertEquals(BoolHelper::is_true_string('Non'), false);
+		$this->assertEquals(false, BoolHelper::is_true_string('False'));
+		$this->assertEquals(false, BoolHelper::is_true_string('No'));
+		$this->assertEquals(false, BoolHelper::is_true_string('Faux'));
+		$this->assertEquals(false, BoolHelper::is_true_string('Non'));
 
-		$this->assertEquals(BoolHelper::is_true_string('FALSE'), false);
-		$this->assertEquals(BoolHelper::is_true_string('NO'), false);
-		$this->assertEquals(BoolHelper::is_true_string('FAUX'), false);
-		$this->assertEquals(BoolHelper::is_true_string('NON'), false);
+		$this->assertEquals(false, BoolHelper::is_true_string('FALSE'));
+		$this->assertEquals(false, BoolHelper::is_true_string('NO'));
+		$this->assertEquals(false, BoolHelper::is_true_string('FAUX'));
+		$this->assertEquals(false, BoolHelper::is_true_string('NON'));
 	}
 
 	public function testToBool()
 	{
-		$this->assertEquals(BoolHelper::to_bool('true'), true);
-		$this->assertEquals(BoolHelper::to_bool('yes'), true);
-		$this->assertEquals(BoolHelper::to_bool('vrai'), true);
-		$this->assertEquals(BoolHelper::to_bool('oui'), true);
+		$this->assertEquals(true, BoolHelper::to_bool('true'));
+		$this->assertEquals(true, BoolHelper::to_bool('yes'));
+		$this->assertEquals(true, BoolHelper::to_bool('vrai'));
+		$this->assertEquals(true, BoolHelper::to_bool('oui'));
 
-		$this->assertEquals(BoolHelper::to_bool('True'), true);
-		$this->assertEquals(BoolHelper::to_bool('Yes'), true);
-		$this->assertEquals(BoolHelper::to_bool('Vrai'), true);
-		$this->assertEquals(BoolHelper::to_bool('Oui'), true);
+		$this->assertEquals(true, BoolHelper::to_bool('True'));
+		$this->assertEquals(true, BoolHelper::to_bool('Yes'));
+		$this->assertEquals(true, BoolHelper::to_bool('Vrai'));
+		$this->assertEquals(true, BoolHelper::to_bool('Oui'));
 
-		$this->assertEquals(BoolHelper::to_bool('TRUE'), true);
-		$this->assertEquals(BoolHelper::to_bool('YES'), true);
-		$this->assertEquals(BoolHelper::to_bool('VRAI'), true);
-		$this->assertEquals(BoolHelper::to_bool('OUI'), true);
+		$this->assertEquals(true, BoolHelper::to_bool('TRUE'));
+		$this->assertEquals(true, BoolHelper::to_bool('YES'));
+		$this->assertEquals(true, BoolHelper::to_bool('VRAI'));
+		$this->assertEquals(true, BoolHelper::to_bool('OUI'));
 
-		$this->assertEquals(BoolHelper::to_bool('1'), true);
-		$this->assertEquals(BoolHelper::to_bool(1), true);
-		$this->assertEquals(BoolHelper::to_bool(0), false);
-		$this->assertEquals(BoolHelper::to_bool('0'), false);
+		$this->assertEquals(true, BoolHelper::to_bool('1'));
+		$this->assertEquals(true, BoolHelper::to_bool(1));
+		$this->assertEquals(true, BoolHelper::to_bool(true));
+		$this->assertEquals(false, BoolHelper::to_bool(false));
+		$this->assertEquals(false, BoolHelper::to_bool(0));
+		$this->assertEquals(false, BoolHelper::to_bool('0'));
 
-		$this->assertEquals(BoolHelper::to_bool('false'), false);
-		$this->assertEquals(BoolHelper::to_bool('no'), false);
-		$this->assertEquals(BoolHelper::to_bool('faux'), false);
-		$this->assertEquals(BoolHelper::to_bool('non'), false);
+		$this->assertEquals(false, BoolHelper::to_bool('false'));
+		$this->assertEquals(false, BoolHelper::to_bool('no'));
+		$this->assertEquals(false, BoolHelper::to_bool('faux'));
+		$this->assertEquals(false, BoolHelper::to_bool('non'));
 
-		$this->assertEquals(BoolHelper::to_bool('False'), false);
-		$this->assertEquals(BoolHelper::to_bool('No'), false);
-		$this->assertEquals(BoolHelper::to_bool('Faux'), false);
-		$this->assertEquals(BoolHelper::to_bool('Non'), false);
+		$this->assertEquals(false, BoolHelper::to_bool('False'));
+		$this->assertEquals(false, BoolHelper::to_bool('No'));
+		$this->assertEquals(false, BoolHelper::to_bool('Faux'));
+		$this->assertEquals(false, BoolHelper::to_bool('Non'));
 
-		$this->assertEquals(BoolHelper::to_bool('FALSE'), false);
-		$this->assertEquals(BoolHelper::to_bool('NO'), false);
-		$this->assertEquals(BoolHelper::to_bool('FAUX'), false);
-		$this->assertEquals(BoolHelper::to_bool('NON'), false);
+		$this->assertEquals(false, BoolHelper::to_bool('FALSE'));
+		$this->assertEquals(false, BoolHelper::to_bool('NO'));
+		$this->assertEquals(false, BoolHelper::to_bool('FAUX'));
+		$this->assertEquals(false, BoolHelper::to_bool('NON'));
 	}
 
 	public function testIsFalseString()
 	{
-		$this->assertEquals(BoolHelper::is_false_string('true'), false);
-		$this->assertEquals(BoolHelper::is_false_string('yes'), false);
-		$this->assertEquals(BoolHelper::is_false_string('vrai'), false);
-		$this->assertEquals(BoolHelper::is_false_string('oui'), false);
+		$this->assertEquals(false, BoolHelper::is_false_string('true'));
+		$this->assertEquals(false, BoolHelper::is_false_string('yes'));
+		$this->assertEquals(false, BoolHelper::is_false_string('vrai'));
+		$this->assertEquals(false, BoolHelper::is_false_string('oui'));
 
-		$this->assertEquals(BoolHelper::is_false_string('True'), false);
-		$this->assertEquals(BoolHelper::is_false_string('Yes'), false);
-		$this->assertEquals(BoolHelper::is_false_string('Vrai'), false);
-		$this->assertEquals(BoolHelper::is_false_string('Oui'), false);
+		$this->assertEquals(false, BoolHelper::is_false_string('True'));
+		$this->assertEquals(false, BoolHelper::is_false_string('Yes'));
+		$this->assertEquals(false, BoolHelper::is_false_string('Vrai'));
+		$this->assertEquals(false, BoolHelper::is_false_string('Oui'));
 
-		$this->assertEquals(BoolHelper::is_false_string('TRUE'), false);
-		$this->assertEquals(BoolHelper::is_false_string('YES'), false);
-		$this->assertEquals(BoolHelper::is_false_string('VRAI'), false);
-		$this->assertEquals(BoolHelper::is_false_string('OUI'), false);
+		$this->assertEquals(false, BoolHelper::is_false_string('TRUE'));
+		$this->assertEquals(false, BoolHelper::is_false_string('YES'));
+		$this->assertEquals(false, BoolHelper::is_false_string('VRAI'));
+		$this->assertEquals(false, BoolHelper::is_false_string('OUI'));
 
-		$this->assertEquals(BoolHelper::is_false_string('1'), false);
-		$this->assertEquals(BoolHelper::is_false_string('0'), true);
+		$this->assertEquals(false, BoolHelper::is_false_string('1'));
+		$this->assertEquals(true, BoolHelper::is_false_string('0'));
 
-		$this->assertEquals(BoolHelper::is_false_string('false'), true);
-		$this->assertEquals(BoolHelper::is_false_string('no'), true);
-		$this->assertEquals(BoolHelper::is_false_string('faux'), true);
-		$this->assertEquals(BoolHelper::is_false_string('non'), true);
+		$this->assertEquals(true, BoolHelper::is_false_string('false'));
+		$this->assertEquals(true, BoolHelper::is_false_string('no'));
+		$this->assertEquals(true, BoolHelper::is_false_string('faux'));
+		$this->assertEquals(true, BoolHelper::is_false_string('non'));
 
-		$this->assertEquals(BoolHelper::is_false_string('False'), true);
-		$this->assertEquals(BoolHelper::is_false_string('No'), true);
-		$this->assertEquals(BoolHelper::is_false_string('Faux'), true);
-		$this->assertEquals(BoolHelper::is_false_string('Non'), true);
+		$this->assertEquals(true, BoolHelper::is_false_string('False'));
+		$this->assertEquals(true, BoolHelper::is_false_string('No'));
+		$this->assertEquals(true, BoolHelper::is_false_string('Faux'));
+		$this->assertEquals(true, BoolHelper::is_false_string('Non'));
 
-		$this->assertEquals(BoolHelper::is_false_string('FALSE'), true);
-		$this->assertEquals(BoolHelper::is_false_string('NO'), true);
-		$this->assertEquals(BoolHelper::is_false_string('FAUX'), true);
-		$this->assertEquals(BoolHelper::is_false_string('NON'), true);
+		$this->assertEquals(true, BoolHelper::is_false_string('FALSE'));
+		$this->assertEquals(true, BoolHelper::is_false_string('NO'));
+		$this->assertEquals(true, BoolHelper::is_false_string('FAUX'));
+		$this->assertEquals(true, BoolHelper::is_false_string('NON'));
 	}
 }
