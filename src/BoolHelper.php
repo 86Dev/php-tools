2a726b37656118ab5e402ae71d043768fc48252c
jtisseau <jonathan.tisseau@86dev.fr>
2020-09-24 18:12:31 +0200

    allow user to include custom true/false strings, is_ok returns null on error, no strict mode in in_array comparison

--- src/BoolHelper.php
+++ src/BoolHelper.php
@@ -12,8 +12,32 @@ namespace PHPTools;
  */
 abstract class BoolHelper
 {
-	protected static $true_strings = ['1', 'ae', 'ano', 'asli', 'bai', 'benetako', 'bəli', 'bẹẹni', 'da', 'dhabta ah', 'e', 'ea sebele', 'echt', 'ee', 'eh', 'eny', 'evet', 'ezigbo', 'fíor', 'gerçek', 'gidi', 'go iawn', 'ha', 'haa', 'halisi', 'haqiqiy', 'ie', 'igen', 'inde', 'iva', 'iya nih', 'ja', 'jah', 'joo', 'já', 'jā', 'ndiyo', 'nekilnojamasis', 'nuhun', 'nyata', 'oo', 'oui', 'po', 'pravi', 'prawdziwy', 'real', 'reali', 'reyèl', 'reāls', 'sebenar', 'si', 'sim', 'skutečný', 'skutočný', 'sì', 'sí', 'taip', 'tak', 'tena', 'thực', 'tinuod nga', 'true', 'tá', 'tõeline', 'tūturu', 'valódi', 'vero', 'vrai', 'vâng', 'weniweni', 'werklike', 'wi', 'ya', 'yebo', 'yes', 'áno', 'ναί', 'πραγματικός', 'да', 'бодит', 'вистински', 'воқеӣ', 'да', 'ды', 'иә', 'прави', 'реален', 'реальний', 'реальный', 'рэальны', 'так', 'тийм ээ', 'шын', 'ҳа', 'այո', 'իրական', 'אמיתי', 'כן', 'פאַקטיש', 'اصلی', 'بله', 'جی ہاں', 'حقيقي', 'نعم فعلا', 'واقعی', 'असली', 'रिअल', 'वास्तविक', 'हाँ', 'हो', 'होय', 'বাস্তব', 'হাঁ', 'ਅਸਲੀ', 'ਹਾਂ', 'વાસ્તવિક', 'હા', 'ஆம்', 'உண்மையான', 'అవును', 'నిజమైన', 'ನಿಜವಾದ', 'ಹೌದು', 'അതെ', 'യഥാർത്ഥ', 'ඔව්', 'සැබෑ', 'จริง', 'ใช่', 'ທີ່ແທ້ຈິງ', 'ແມ່ນແລ້ວ', 'စစ်မှန်သော', 'ဟုတ်ကဲ့', 'დიახ', 'რეალური', 'បាទ', 'ពិតប្រាកដ', 'はい', 'リアル', '实', '實', '是', '예', '현실'];
-	protected static $false_strings = ['0', '', 'amanga', 'ayi', 'babu', 'bakak', 'been ah', 'bohata', 'br', 'bréagach', 'cha', 'che', 'dili', 'dim', 'diso', 'ee e', 'ei', 'eke', 'fals', 'falsch', 'false', 'falsk', 'falso', 'faltsua', 'falz', 'faux', 'fałszywy', 'ffug', 'fo', 'geen', 'hamis', 'hapana', 'hindi', 'huwad', 'ingen', 'jo', 'không', 'klaidinga', 'lažan', 'lažno', 'le', 'maya', 'napačen', 'ne', 'nee', 'nei', 'nein', 'nej', 'nem', 'nepravdivé', 'nepravdivý', 'nie', 'no', 'non', 'noto\'g\'ri', 'nr', 'nu', 'nē', 'onwaar', 'ora', 'pa gen okenn', 'palsu', 'rangt', 'rara', 'sai', 'salah', 'teka', 'teu', 'tidak', 'tsy misy', 'uimh', 'uongo', 'vale', 'vals', 'viltus', 'väärä', 'yanlış', 'yo\'q', 'yok hayır', 'yox', 'zabodza', 'št', 'žiadny', 'ƙarya', 'ψευδής', 'όχι', 'не', 'бр', 'жалған', 'жоқ', 'лажно', 'ложный', 'не', 'немає', 'нет', 'неточно', 'нодуруст', 'няма', 'помилковий', 'фалшив', 'фальшывы', 'худал', 'үгүй', 'կեղծ', 'ոչ', 'לא', 'שֶׁקֶר', 'خاطئة', 'غلط', 'لا', 'نادرست', 'نه', 'نہیں', 'असत्य', 'खोटे', 'गलत', 'नहीं', 'नाही', 'না', 'মিথ্যা', 'ਝੂਠ', 'ਨਹੀਂ', 'ખોટા', 'ના', 'இல்லை', 'தவறான', 'ఏ', 'తప్పుడు', 'ಇಲ್ಲ', 'ಸುಳ್ಳು', 'ഇല്ല', 'തെറ്റായ', 'අසත්යය', 'නැත', 'เท็จ', 'ไม่', 'ບໍ່ຖືກຕ້ອງ', 'မမှန်သော', 'အဘယ်သူမျှမ', 'არა', 'ყალბი', 'ទេ', 'មិនពិត', 'ụgha', '假', '偽', '沒有', '没有', '그릇된', '아니'];
+	protected static $true_strings = ['ae', 'ano', 'asli', 'bai', 'benetako', 'bəli', 'bẹẹni', 'da', 'dhabta ah', 'e', 'ea sebele', 'echt', 'ee', 'eh', 'eny', 'evet', 'ezigbo', 'fíor', 'gerçek', 'gidi', 'go iawn', 'ha', 'haa', 'halisi', 'haqiqiy', 'ie', 'igen', 'inde', 'iva', 'iya nih', 'ja', 'jah', 'joo', 'já', 'jā', 'ndiyo', 'nekilnojamasis', 'nuhun', 'nyata', 'oo', 'oui', 'po', 'pravi', 'prawdziwy', 'real', 'reali', 'reyèl', 'reāls', 'sebenar', 'si', 'sim', 'skutečný', 'skutočný', 'sì', 'sí', 'taip', 'tak', 'tena', 'thực', 'tinuod nga', 'true', 'tá', 'tõeline', 'tūturu', 'valódi', 'vero', 'vrai', 'vâng', 'weniweni', 'werklike', 'wi', 'ya', 'yebo', 'yes', 'áno', 'ναί', 'πραγματικός', 'да', 'бодит', 'вистински', 'воқеӣ', 'да', 'ды', 'иә', 'прави', 'реален', 'реальний', 'реальный', 'рэальны', 'так', 'тийм ээ', 'шын', 'ҳа', 'այո', 'իրական', 'אמיתי', 'כן', 'פאַקטיש', 'اصلی', 'بله', 'جی ہاں', 'حقيقي', 'نعم فعلا', 'واقعی', 'असली', 'रिअल', 'वास्तविक', 'हाँ', 'हो', 'होय', 'বাস্তব', 'হাঁ', 'ਅਸਲੀ', 'ਹਾਂ', 'વાસ્તવિક', 'હા', 'ஆம்', 'உண்மையான', 'అవును', 'నిజమైన', 'ನಿಜವಾದ', 'ಹೌದು', 'അതെ', 'യഥാർത്ഥ', 'ඔව්', 'සැබෑ', 'จริง', 'ใช่', 'ທີ່ແທ້ຈິງ', 'ແມ່ນແລ້ວ', 'စစ်မှန်သော', 'ဟုတ်ကဲ့', 'დიახ', 'რეალური', 'បាទ', 'ពិតប្រាកដ', 'はい', 'リアル', '实', '實', '是', '예', '현실'];
+	protected static $false_strings = ['0', 'amanga', 'ayi', 'babu', 'bakak', 'been ah', 'bohata', 'br', 'bréagach', 'cha', 'che', 'dili', 'dim', 'diso', 'ee e', 'ei', 'eke', 'fals', 'falsch', 'false', 'falsk', 'falso', 'faltsua', 'falz', 'faux', 'fałszywy', 'ffug', 'fo', 'geen', 'hamis', 'hapana', 'hindi', 'huwad', 'ingen', 'jo', 'không', 'klaidinga', 'lažan', 'lažno', 'le', 'maya', 'napačen', 'ne', 'nee', 'nei', 'nein', 'nej', 'nem', 'nepravdivé', 'nepravdivý', 'nie', 'no', 'non', 'noto\'g\'ri', 'nr', 'nu', 'nē', 'onwaar', 'ora', 'pa gen okenn', 'palsu', 'rangt', 'rara', 'sai', 'salah', 'teka', 'teu', 'tidak', 'tsy misy', 'uimh', 'uongo', 'vale', 'vals', 'viltus', 'väärä', 'yanlış', 'yo\'q', 'yok hayır', 'yox', 'zabodza', 'št', 'žiadny', 'ƙarya', 'ψευδής', 'όχι', 'не', 'бр', 'жалған', 'жоқ', 'лажно', 'ложный', 'не', 'немає', 'нет', 'неточно', 'нодуруст', 'няма', 'помилковий', 'фалшив', 'фальшывы', 'худал', 'үгүй', 'կեղծ', 'ոչ', 'לא', 'שֶׁקֶר', 'خاطئة', 'غلط', 'لا', 'نادرست', 'نه', 'نہیں', 'असत्य', 'खोटे', 'गलत', 'नहीं', 'नाही', 'না', 'মিথ্যা', 'ਝੂਠ', 'ਨਹੀਂ', 'ખોટા', 'ના', 'இல்லை', 'தவறான', 'ఏ', 'తప్పుడు', 'ಇಲ್ಲ', 'ಸುಳ್ಳು', 'ഇല്ല', 'തെറ്റായ', 'අසත්යය', 'නැත', 'เท็จ', 'ไม่', 'ບໍ່ຖືກຕ້ອງ', 'မမှန်သော', 'အဘယ်သူမျှမ', 'არა', 'ყალბი', 'ទេ', 'មិនពិត', 'ụgha', '假', '偽', '沒有', '没有', '그릇된', '아니'];
+
+	/**
+	 * Add a true string to the list of true strings
+	 *
+	 * @param string $true_string
+	 *
+	 * @return void
+	 */
+	public static function add_true_string($true_string)
+	{
+		static::$true_strings[] = strtolower($true_string);
+	}
+
+	/**
+	 * Add a false string to the list of false strings
+	 *
+	 * @param string $false_string
+	 *
+	 * @return void
+	 */
+	public static function add_false_string($false_string)
+	{
+		static::$false_strings[] = strtolower($false_string);
+	}
 
 	/**
 	 * Convert a string to boolean
@@ -32,20 +56,21 @@ abstract class BoolHelper
 			return false;
 		}
 
-
-		return self::is_true_string($value);
+		return static::is_true_string($value);
 	}
 
 	/**
-	 * Get a value to boolean
+	 * Get a value to boolean.
+	 *
+	 * Use filter_var so only english strings works. For non english strings, use BoolHelper::to_bool().
 	 *
 	 * @param mixed $value Value to check
 	 *
-	 * @return bool
+	 * @return bool|null
 	 */
 	public static function is_ok($value)
 	{
-		return filter_var($value, FILTER_VALIDATE_BOOLEAN);
+		return filter_var($value, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
 	}
 
 	/**
@@ -57,7 +82,7 @@ abstract class BoolHelper
 	 */
 	public static function is_true_string($string)
 	{
-		return in_array(mb_strtolower((string)$string), static::$true_strings, true);
+		return in_array(mb_strtolower((string)$string), static::$true_strings);
 	}
 
 	/**
@@ -69,6 +94,6 @@ abstract class BoolHelper
 	 */
 	public static function is_false_string($string)
 	{
-		return empty($string) || in_array(mb_strtolower($string), static::$false_strings, true);
+		return empty($string) || in_array(mb_strtolower($string), static::$false_strings);
 	}
 }
--- tests/BoolHelperTest.php
+++ tests/BoolHelperTest.php
@@ -8,7 +8,7 @@ final class BoolHelperTest extends TestCase
 {
 	protected $true_scalars = [true, 1, 2, -1, -2, 1.001, -1.001];
 	protected $false_scalars = [false, 0, 0.0, null];
-	protected $true_strings = ['1', 'ae', 'ano', 'asli', 'bai', 'benetako', 'bəli', 'bẹẹni', 'da', 'dhabta ah', 'e', 'ea sebele', 'echt', 'ee', 'eh', 'eny', 'evet', 'ezigbo', 'fíor', 'gerçek', 'gidi', 'go iawn', 'ha', 'haa', 'halisi', 'haqiqiy', 'ie', 'igen', 'inde', 'iva', 'iya nih', 'ja', 'jah', 'joo', 'já', 'jā', 'ndiyo', 'nekilnojamasis', 'nuhun', 'nyata', 'oo', 'oui', 'po', 'pravi', 'prawdziwy', 'real', 'reali', 'reyèl', 'reāls', 'sebenar', 'si', 'sim', 'skutečný', 'skutočný', 'sì', 'sí', 'taip', 'tak', 'tena', 'thực', 'tinuod nga', 'true', 'tá', 'tõeline', 'tūturu', 'valódi', 'vero', 'vrai', 'vâng', 'weniweni', 'werklike', 'wi', 'ya', 'yebo', 'yes', 'áno', 'ναί', 'πραγματικός', 'да', 'бодит', 'вистински', 'воқеӣ', 'да', 'ды', 'иә', 'прави', 'реален', 'реальний', 'реальный', 'рэальны', 'так', 'тийм ээ', 'шын', 'ҳа', 'այո', 'իրական', 'אמיתי', 'כן', 'פאַקטיש', 'اصلی', 'بله', 'جی ہاں', 'حقيقي', 'نعم فعلا', 'واقعی', 'असली', 'रिअल', 'वास्तविक', 'हाँ', 'हो', 'होय', 'বাস্তব', 'হাঁ', 'ਅਸਲੀ', 'ਹਾਂ', 'વાસ્તવિક', 'હા', 'ஆம்', 'உண்மையான', 'అవును', 'నిజమైన', 'ನಿಜವಾದ', 'ಹೌದು', 'അതെ', 'യഥാർത്ഥ', 'ඔව්', 'සැබෑ', 'จริง', 'ใช่', 'ທີ່ແທ້ຈິງ', 'ແມ່ນແລ້ວ', 'စစ်မှန်သော', 'ဟုတ်ကဲ့', 'დიახ', 'რეალური', 'បាទ', 'ពិតប្រាកដ', 'はい', 'リアル', '实', '實', '是', '예', '현실'];
+	protected $true_strings = ['ae', 'ano', 'asli', 'bai', 'benetako', 'bəli', 'bẹẹni', 'da', 'dhabta ah', 'e', 'ea sebele', 'echt', 'ee', 'eh', 'eny', 'evet', 'ezigbo', 'fíor', 'gerçek', 'gidi', 'go iawn', 'ha', 'haa', 'halisi', 'haqiqiy', 'ie', 'igen', 'inde', 'iva', 'iya nih', 'ja', 'jah', 'joo', 'já', 'jā', 'ndiyo', 'nekilnojamasis', 'nuhun', 'nyata', 'oo', 'oui', 'po', 'pravi', 'prawdziwy', 'real', 'reali', 'reyèl', 'reāls', 'sebenar', 'si', 'sim', 'skutečný', 'skutočný', 'sì', 'sí', 'taip', 'tak', 'tena', 'thực', 'tinuod nga', 'true', 'tá', 'tõeline', 'tūturu', 'valódi', 'vero', 'vrai', 'vâng', 'weniweni', 'werklike', 'wi', 'ya', 'yebo', 'yes', 'áno', 'ναί', 'πραγματικός', 'да', 'бодит', 'вистински', 'воқеӣ', 'да', 'ды', 'иә', 'прави', 'реален', 'реальний', 'реальный', 'рэальны', 'так', 'тийм ээ', 'шын', 'ҳа', 'այո', 'իրական', 'אמיתי', 'כן', 'פאַקטיש', 'اصلی', 'بله', 'جی ہاں', 'حقيقي', 'نعم فعلا', 'واقعی', 'असली', 'रिअल', 'वास्तविक', 'हाँ', 'हो', 'होय', 'বাস্তব', 'হাঁ', 'ਅਸਲੀ', 'ਹਾਂ', 'વાસ્તવિક', 'હા', 'ஆம்', 'உண்மையான', 'అవును', 'నిజమైన', 'ನಿಜವಾದ', 'ಹೌದು', 'അതെ', 'യഥാർത്ഥ', 'ඔව්', 'සැබෑ', 'จริง', 'ใช่', 'ທີ່ແທ້ຈິງ', 'ແມ່ນແລ້ວ', 'စစ်မှန်သော', 'ဟုတ်ကဲ့', 'დიახ', 'რეალური', 'បាទ', 'ពិតប្រាកដ', 'はい', 'リアル', '实', '實', '是', '예', '현실'];
 	protected $false_strings = ['0', '', 'amanga', 'ayi', 'babu', 'bakak', 'been ah', 'bohata', 'br', 'bréagach', 'cha', 'che', 'dili', 'dim', 'diso', 'ee e', 'ei', 'eke', 'fals', 'falsch', 'false', 'falsk', 'falso', 'faltsua', 'falz', 'faux', 'fałszywy', 'ffug', 'fo', 'geen', 'hamis', 'hapana', 'hindi', 'huwad', 'ingen', 'jo', 'không', 'klaidinga', 'lažan', 'lažno', 'le', 'maya', 'napačen', 'ne', 'nee', 'nei', 'nein', 'nej', 'nem', 'nepravdivé', 'nepravdivý', 'nie', 'no', 'non', 'noto\'g\'ri', 'nr', 'nu', 'nē', 'onwaar', 'ora', 'pa gen okenn', 'palsu', 'rangt', 'rara', 'sai', 'salah', 'teka', 'teu', 'tidak', 'tsy misy', 'uimh', 'uongo', 'vale', 'vals', 'viltus', 'väärä', 'yanlış', 'yo\'q', 'yok hayır', 'yox', 'zabodza', 'št', 'žiadny', 'ƙarya', 'ψευδής', 'όχι', 'Не', 'бр', 'жалған', 'жоқ', 'лажно', 'ложный', 'не', 'немає', 'нет', 'неточно', 'нодуруст', 'няма', 'помилковий', 'фалшив', 'фальшывы', 'худал', 'Үгүй', 'կեղծ', 'ոչ', 'לא', 'שֶׁקֶר', 'خاطئة', 'غلط', 'لا', 'نادرست', 'نه', 'نہیں', 'असत्य', 'खोटे', 'गलत', 'नहीं', 'नाही', 'না', 'মিথ্যা', 'ਝੂਠ', 'ਨਹੀਂ', 'ખોટા', 'ના', 'இல்லை', 'தவறான', 'ఏ', 'తప్పుడు', 'ಇಲ್ಲ', 'ಸುಳ್ಳು', 'ഇല്ല', 'തെറ്റായ', 'අසත්යය', 'නැත', 'เท็จ', 'ไม่', 'ບໍ່ຖືກຕ້ອງ', 'မမှန်သော', 'အဘယ်သူမျှမ', 'არა', 'ყალბი', 'ទេ', 'មិនពិត', 'ụgha', '假', '偽', '沒有', '没有', '그릇된', '아니'];
 
 	public function testIsTrueString()
@@ -16,12 +16,10 @@ final class BoolHelperTest extends TestCase
 		foreach ($this->true_strings as $string)
 		{
 			$this->assertTrue(BoolHelper::is_true_string($string), "'$string' is false, should be true.");
-			// $this->assertTrue(BoolHelper::is_true_string($up = mb_strtoupper($string)), "'$up' is false, should be true.");
 		}
 		foreach ($this->false_strings as $string)
 		{
 			$this->assertFalse(BoolHelper::is_true_string($string), "'$string' is true, should be false.");
-			// $this->assertFalse(BoolHelper::is_true_string($up = mb_strtoupper($string)), "'$up' is true, should be false.");
 		}
 	}
 
@@ -30,7 +28,6 @@ final class BoolHelperTest extends TestCase
 		foreach ($this->true_strings as $string)
 		{
 			$this->assertTrue(BoolHelper::to_bool($string), "'$string' is false, should be true.");
-			// $this->assertTrue(BoolHelper::to_bool($up = mb_strtoupper($string)), "'$up' is false, should be true.");
 		}
 		foreach ($this->true_scalars as $scalar)
 		{
@@ -39,7 +36,6 @@ final class BoolHelperTest extends TestCase
 		foreach ($this->false_strings as $string)
 		{
 			$this->assertFalse(BoolHelper::to_bool($string), "'$string' is true, should be false.");
-			// $this->assertFalse(BoolHelper::to_bool($up = mb_strtoupper($string)), "'$up' is true, should be false.");
 		}
 		foreach ($this->false_scalars as $scalar)
 		{
@@ -59,5 +55,19 @@ final class BoolHelperTest extends TestCase
 			$this->assertTrue(BoolHelper::is_false_string($string), "'$string' is true, should be false.");
 			// $this->assertTrue(BoolHelper::is_false_string($up = mb_strtoupper($string)), "'$up' is true, should be false.");
 		}
+	}
+
+	public function testAddFalseString()
+	{
+		BoolHelper::add_false_string('N');
+		$this->assertTrue(BoolHelper::is_false_string('n'), "'n' is true, should be false.");
+		$this->assertTrue(BoolHelper::is_false_string('N'), "'N' is true, should be false.");
+	}
+
+	public function testAddTrueString()
+	{
+		BoolHelper::add_true_string('Y');
+		$this->assertTrue(BoolHelper::is_true_string('y'), "'y' is false, should be true.");
+		$this->assertTrue(BoolHelper::is_true_string('Y'), "'Y' is false, should be true.");
 	}
 }
